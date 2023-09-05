70841ef180dd696c9bf92ba4eb868e9ab153691b
EmilianoEmanuelSosa <emilianoemauel2850@gmail.com>
2023-09-05 16:30:56 -0300

    <mf>restaurant app

--- server/restaurant_api_project/load_csv.py
+++ server/restaurant_api_project/load_csv.py
@@ -1,40 +1,94 @@
-from .models import Place # Asegúrate de ajustar la ruta al modelo Place
+from .models import Restaurant, User, Review
 import csv
+from decimal import Decimal
+from django.utils.dateparse import parse_date
 
-# Ruta al archivo CSV
-csv_file_path_1 = '/home/mkm/programin/Marke_Analysis_Project_Google/Data/Data_process/restaurants.csv'
-csv_file_path_2 = '/home/mkm/programin/Marke_Analysis_Project_Google/Data/Data_process/reviews.csv'
-csv_file_path_3 = '/home/mkm/programin/Marke_Analysis_Project_Google/Data/Data_process/users.csv'
+# Rutas a los archivos CSV
+csv_file_path_restaurants = '/home/mkm/programin/Marke_Analysis_Project_Google/Data/Data_process/restaurants.csv'
+csv_file_path_reviews = '/home/mkm/programin/Marke_Analysis_Project_Google/Data/Data_process/reviews.csv'
+csv_file_path_users = '/home/mkm/programin/Marke_Analysis_Project_Google/Data/Data_process/users.csv'
 
-Data/Data_process/restaurants.csv
-Data/Data_process/reviews.csv
-Data/Data_process/users.csv
+def import_restaurants_from_csv(file_path):
+    with open(file_path, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        restaurants_to_create = []
 
-BATCH_SIZE = 100
-
-with open(csv_file_path, 'r') as file:
-    csv_reader = csv.DictReader(file)
-    places_to_create = []
-
-    for row in csv_reader:
-        # Verifica si ya existe un lugar con el mismo nombre en la base de datos
-        existing_place = Place.objects.filter(name=row['name']).first()
-
-        if not existing_place:
-            place = Place(
+        for row in csv_reader:
+            restaurant = Restaurant(
                 name=row['name'],
-                latitude=row['latitude'],
-                longitude=row['longitude'],
-                category=row['category'],
-                avg_rating=row['avg_rating'],
-                num_of_reviews=row['num_of_reviews']
+                address=row['address'],
+                latitude=Decimal(row['latitude']),
+                longitude=Decimal(row['longitude']),
+                rating=Decimal(row['rating']),
+                review_count=int(row['review_count']),
+                categories=row['categories']
             )
-            places_to_create.append(place)
+            restaurants_to_create.append(restaurant)
 
-            if len(places_to_create) == BATCH_SIZE:
-                Place.objects.bulk_create(places_to_create)
-                places_to_create = []
+            if len(restaurants_to_create) == BATCH_SIZE:
+                Restaurant.objects.bulk_create(restaurants_to_create)
+                restaurants_to_create = []
 
-    # Inserta cualquier remanente que quede en el último lote
-    if places_to_create:
-        Place.objects.bulk_create(places_to_create)
+        # Inserta cualquier remanente que quede en el último lote
+        if restaurants_to_create:
+            Restaurant.objects.bulk_create(restaurants_to_create)
+
+
+
+def import_reviews_from_csv(file_path):
+    with open(file_path, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        reviews_to_create = []
+
+        for row in csv_reader:
+            user_id = row['user_id']  # Mantén user_id como cadena
+            restaurant_id = row['restaurant_id']  # Mantén restaurant_id como cadena
+
+            # No es necesario verificar si el usuario o el restaurante existen previamente
+
+            review = Review(
+                user_id=user_id,
+                restaurant_id=restaurant_id,
+                stars=Decimal(row['stars']),  # Utiliza 'stars' en lugar de 'rating'
+                text=row['text'],
+                date=parse_date(row['date'])  # Utiliza 'date' en lugar de 'time'
+            )
+            reviews_to_create.append(review)
+
+            if len(reviews_to_create) == BATCH_SIZE:
+                Review.objects.bulk_create(reviews_to_create)
+                reviews_to_create = []
+
+        # Inserta cualquier remanente que quede en el último lote
+        if reviews_to_create:
+            Review.objects.bulk_create(reviews_to_create)
+
+# Llama a la función para importar las revisiones desde el archivo CSV
+import_reviews_from_csv(csv_file_path_reviews)
+
+def import_users_from_csv(file_path):
+    with open(file_path, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        users_to_create = []
+
+        for row in csv_reader:
+            user = User(
+                name=row['name'],
+                registration_date=parse_date(row['registration_date'])
+            )
+            users_to_create.append(user)
+
+            if len(users_to_create) == BATCH_SIZE:
+                User.objects.bulk_create(users_to_create)
+                users_to_create = []
+
+        # Inserta cualquier remanente que quede en el último lote
+        if users_to_create:
+            User.objects.bulk_create(users_to_create)
+
+BATCH_SIZE = 3000  # Ajusta el tamaño del lote según tus necesidades
+
+# Llama a las funciones para importar datos desde los archivos CSV
+import_restaurants_from_csv(csv_file_path_restaurants)
+import_reviews_from_csv(csv_file_path_reviews)
+import_users_from_csv(csv_file_path_users)
--- server/restaurant_api_project/models.py
+++ server/restaurant_api_project/models.py
@@ -2,23 +2,23 @@ from django.db import models
 
 class Restaurant(models.Model):
     restaurant_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=256)
-    address = models.CharField(max_length=256, blank=True, null=True)
+    name = models.CharField(max_length=512)
+    address = models.CharField(max_length=512, blank=True, null=True)
     latitude = models.FloatField()
     longitude = models.FloatField()
     rating = models.DecimalField(max_digits=3, decimal_places=2)
     review_count = models.IntegerField()
-    categories = models.CharField(max_length=256)
+    categories = models.CharField(max_length=512)
+
 
 class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=256)
-    registration_date = models.DateField()
+    user_id = models.CharField(max_length=512, primary_key=True)
+    name = models.CharField(max_length=512)
+
 
 class Review(models.Model):
-    review_id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
-    rating = models.IntegerField(null=True)
-    text = models.TextField(null=True)
-    time = models.DateField()
+    user_id = models.CharField(max_length=512)
+    restaurant_id = models.CharField(max_length=512)
+    stars = models.DecimalField(max_digits=3, decimal_places=1)
+    text = models.TextField()
+    date = models.DateField()
