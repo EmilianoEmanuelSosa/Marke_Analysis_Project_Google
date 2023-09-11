dc049515fd43126e2776a83dd2dd14abab9427dc
EmilianoEmanuelSosa <emilianoemauel2850@gmail.com>
2023-09-11 19:34:35 -0300

    <add>dockerization

--- server/restaurant_api_project/urls.py
+++ server/restaurant_api_project/urls.py
@@ -3,7 +3,7 @@ from .api import RestaurantViewSet,UserViewSet
 
 urlpatterns = [
     # Otras rutas...
-    path('restaurantes_id/<str:restaurant_id>/', RestaurantViewSet.as_view({'post': 'retrieve'}), name='restaurant-detail'),
+    path('restaurantes_id/<str:restaurant_id>/', RestaurantViewSet.as_view({'get': 'retrieve'}), name='restaurant-detail'),
     path('restaurantes_en_radio/<str:restaurant_id>/<int:radius_km>/', RestaurantViewSet.as_view({'get': 'get_restaurants_in_radius'}), name='restaurants-in-radius'),
     path('restaurantes_reviews/<str:restaurant_id>/', RestaurantViewSet.as_view({'get': 'get_reviews_for_restaurant'}), name='restaurant-reviews'),
     path('usuarios/<str:user_id>/', UserViewSet.as_view({'get': 'retrieve'}), name='user-detail')
